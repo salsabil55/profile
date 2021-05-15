3764afa3a03ac5c0464613155885ecf71754dced
salsabil_55 <salsabil.tarek@dafholding.com>
2021-05-15 20:15:25 +0200

    files

--- js/main.js
+++ js/main.js
@@ -22,6 +22,24 @@ $(window).load(function () {
   $(".loader").fadeOut("2000");
   $(".loader").addClass("load");
 });
+
+// lightbox
+$(document).ready(function () {
+  // When the button is clicked make the lightbox fade in in the span of 1 second, hide itself and start the video
+  $(".button").on("click", function () {
+    $("#lightbox").fadeIn(1000);
+    $("#button").hide();
+    var videoURL = $("#video").prop("src");
+    videoURL += "?autoplay=1";
+    $("#video").prop("src", videoURL);
+  });
+
+  // When the close button is clicked make the lightbox fade out in the span of 0.5 seconds and show the play button
+  $("#close-btn").on("click", function () {
+    $("#lightbox").fadeOut(500);
+    $("#button").show(250);
+  });
+});
 // canvas
 
 function RSquare(c, w, h, cx, cy) {
